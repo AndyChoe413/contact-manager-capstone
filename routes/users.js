ced56bdadd950efdf920aec901688f442c817a60
Andy Choe <Andrew.s.choe@gmail.com>
2021-12-01 11:53:47 -0500

    testing

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const router = express.Router()
+// const validator = require('validator')
+const {check, validationResult} = require('express-validator')
 
 //schema
 const User = require('../models/User')
 
-router.post('/', (req, res) => {
-    res.send(req.body)
+router.post('/', [
+    check('name', 'a name is required').not().isEmpty(),
+    check('email', 'please enter a valid email').isEmail(),
+    check('password', 'please ender a password with 6 or more characters').isLength({min: 6})
+],
+    (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors:errors.array()})
+        }
+        res.send('passed')
 })
 
 module.exports = router
